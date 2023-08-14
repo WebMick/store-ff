d290b55f4fc3d950a91401fdcb8ef43de2350c68
webmick <438247026@qq.com>
2023-08-14 15:20:21 +0800

    Initial commit

---
+++ assets/invite-s.5b72cabc.js
@@ -0,0 +1,1 @@
+const s="/assets/invite-l-9323ab40.png",a="/assets/invite-mm-0a848f3c.png",e="/assets/invite-s-e3d7a853.png";export{s as _,a,e as b};
---
+++ assets/useForm.fc8de6ee.js
@@ -0,0 +1,1 @@
+function t(){return{verifyPhone:function(t){return/^[0-9]{1,24}$/.test(t)},verifyEmail:function(t){return/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(t)}}}export{t as u};
